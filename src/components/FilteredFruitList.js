1ff48ed7a46fac9f4317d8296b3c598d83121b21
Neyan najat <nyannajat8@gmail.com>
2018-09-22 08:47:33 +0000

    automatically backed up by learn

---
+++ src/components/FilteredFruitList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ const FilteredFruitList = ({ fruit, filter }) => {
+  const fruitList = !filter ?  fruit : fruit.filter(item => item.fruit_type === filter);
+   return (
+    <ul className="fruit-list">
+      {fruitList.map((fruit, index) => <li key={index}>{fruit.char}</li>)}
+    </ul>
+  );
+};
+ FilteredFruitList.defaultProps = {
+  fruit: [],
+  filter: null
+};
+ export default FilteredFruitList; 
